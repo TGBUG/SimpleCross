5c54b6c3fc7c4aaa517fa1fe17eca73dc792005d
TGBUG <3770955706@qq.com>
2025-01-23 19:32:22 +0800

    首次提交

---
+++ src/main/java/TGBUG/simpleCross/SimpleCross.java
@@ -0,0 +1,17 @@
+package TGBUG.simpleCross;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleCross extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
